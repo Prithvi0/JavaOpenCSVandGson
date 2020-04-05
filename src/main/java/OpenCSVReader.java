c50e2ffdaab59ea1860092ae977a46aa08c59470
prithvi0 <prithviy96@gmail.com>
2020-04-06 01:54:20 +0530

    Added updated gradle dependency file & program to read .csv file

--- src/main/java/OpenCSVReader.java
+++ src/main/java/OpenCSVReader.java
@@ -1,5 +1,30 @@
+import com.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class OpenCSVReader {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Java Open Source Libraries - OpenCSV and Gson");
+    private static final String CSV_FILE_PATH = "./src/main/resources/users_details.csv";
+
+    public static void main(String[] args) throws IOException {
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(CSV_FILE_PATH));
+            CSVReader csvReader = new CSVReader(reader);
+            {
+                // READING RECORDS ONE BY ONE IN A STRING ARRAY
+                String[] nextRecord;
+                while ((nextRecord = csvReader.readNext()) != null) {
+                    System.out.println("Name : " + nextRecord[0]);
+                    System.out.println("Email : " + nextRecord[1]);
+                    System.out.println("Phone : " + nextRecord[2]);
+                    System.out.println("Country : " + nextRecord[3]);
+                    System.out.println("--------------------------------");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
