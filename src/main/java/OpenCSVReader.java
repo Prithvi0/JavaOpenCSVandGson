0410042d639e72ea7c49d5ddefdec7bdfcea036c
prithvi0 <prithviy96@gmail.com>
2020-04-06 02:09:48 +0530

    Added program to read all the records at once into list array

--- src/main/java/OpenCSVReader.java
+++ src/main/java/OpenCSVReader.java
@@ -1,9 +1,11 @@
 import com.opencsv.CSVReader;
+import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class OpenCSVReader {
     private static final String CSV_FILE_PATH = "./src/main/resources/users_details.csv";
@@ -13,13 +15,13 @@ public class OpenCSVReader {
             Reader reader = Files.newBufferedReader(Paths.get(CSV_FILE_PATH));
             CSVReader csvReader = new CSVReader(reader);
             {
-                // READING RECORDS ONE BY ONE IN A STRING ARRAY
-                String[] nextRecord;
-                while ((nextRecord = csvReader.readNext()) != null) {
-                    System.out.println("Name : " + nextRecord[0]);
-                    System.out.println("Email : " + nextRecord[1]);
-                    System.out.println("Phone : " + nextRecord[2]);
-                    System.out.println("Country : " + nextRecord[3]);
+                // READING ALL THE RECORDS AT ONCE BY ONE INTO A LIST ARRAY
+                List<String[]> records = csvReader.readAll();
+                for (String[] record : records) {
+                    System.out.println("Name : " + record[0]);
+                    System.out.println("Email : " + record[1]);
+                    System.out.println("Phone : " + record[2]);
+                    System.out.println("Country : " + record[3]);
                     System.out.println("--------------------------------");
                 }
             }
